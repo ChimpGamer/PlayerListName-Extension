cd4bb9a2b73570cf1e142f2bde1aa2b853a98ddb
ChimpGamer <Tim.Cremers@Ziggo.nl>
2022-05-02 18:26:17 +0200

    Updated shadow to 7.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.6.21"
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "nl.chimpgamer.networkmanager.extensions"
